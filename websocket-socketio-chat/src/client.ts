40328e2dc538e74e67188097efd2833260c8c2da
Ivan Obeso <ivan.obeso1@gmail.com>
2022-08-18 17:32:14 +0100

    some more functionality

--- websocket-socketio-chat/src/client.ts
+++ websocket-socketio-chat/src/client.ts
@@ -1,25 +1,47 @@
 import {io} from 'socket.io-client'
-const prompt = require('prompt-sync')({sigint: true});
 const readline = require('readline');
+
+const rooms: string[] = [];
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
-/**
- * Listener for reading the user lines
- */
-rl.on('line', (line: any) => {
-    process.stdout.write('>> ');
-})
+const displayRooms = () => console.log(`Connected to rooms: ${rooms}`);
 
 const socket = io('http://localhost:3000')
 socket.on('connect', () => {
     console.log(`You connected with ID=${socket.id}`)
     process.stdout.write('>> ');
+    /**
+     * Listener for reading the user lines
+     */
+    rl.on('line', (line: any) => {
+        if(line.startsWith('JOIN ROOM ')){
+            const room = line.split(' ')[2]
+            rooms.push(room);
+            displayRooms();
+            socket.emit('join-room', room);
+        }
+        else if(line.startsWith('ROOM ')){
+            const split = line.split(' ');
+            socket.emit('send-message', split[2], split[1])
+        } 
+        else{
+            socket.emit('send-message', line)
+        }
+        process.stdout.write('>> ');
+    })
 });
 
-
+socket.on('received-message', (message: string, fromUser: string, room: string) => {
+    console.log(`[${fromUser}]: ${message}`, (room)?`(to room ${room})`:'');
+    process.stdout.write('>> ');
+})
 
 console.log('Running chat client')
+console.log('If you want to join a room type: JOIN ROOM <name_of_room>')
+console.log('If you want to send a message to a room type: ROOM <name_of_room> <message>')
+displayRooms();
--- websocket-socketio-chat/src/server.ts
+++ websocket-socketio-chat/src/server.ts
@@ -3,6 +3,21 @@ const io = require("socket.io")(3000, options);
 
 io.on("connection", (socket: any) => {
     console.log(`Client with ID=${socket.id} connected`);
+    socket.on('send-message', (message: string, room: string) => {
+        console.log(`Message received from ${socket.id}: ${message}`, (room)?`(to room ${room})`:'');
+        if(room){
+            /* The message is to be direvted to some specific room */
+            socket.to(room).emit('received-message', message, socket.id, room)
+        }else{
+            /* I will take the message that came from that particular user and
+            broadcast it to all the clients connected */
+            socket.broadcast.emit('received-message', message, socket.id)
+        }
+    })
+    socket.on('join-room', (room: string) => {
+        console.log(`User: ${socket.id} joined the room: ${room}`);
+        socket.join(room);
+    });
 })
 
 console.log('Running chat server')
